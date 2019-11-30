53257b698a69105296b4b10fbb485f82d8fab374
wind711 <wind711@students.cs.ubc.ca>
2019-11-30 15:33:50 -0800

    Initial commit

--- app/src/main/java/com/app/tap/MainActivity.java
+++ app/src/main/java/com/app/tap/MainActivity.java
@@ -2,11 +2,14 @@ package com.app.tap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.app.tap.handler.HexHelper;
@@ -18,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent databaseIntent = new
+                        Intent(getApplicationContext(), Database.class);
+                startActivity(databaseIntent);
+            }
+        });
     }
 }
